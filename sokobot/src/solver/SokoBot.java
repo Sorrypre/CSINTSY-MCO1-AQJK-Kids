9d46f4a711947aa3142a2e4a9efb3bf3c65ec5da
Sorrypre <jjlespada5@gmail.com>
2025-09-25 20:53:19 +0800

    Added initial files in the repository

---
+++ sokobot/src/solver/SokoBot.java
@@ -0,0 +1,17 @@
+package solver;
+
+public class SokoBot {
+
+  public String solveSokobanPuzzle(int width, int height, char[][] mapData, char[][] itemsData) {
+    /*
+     * YOU NEED TO REWRITE THE IMPLEMENTATION OF THIS METHOD TO MAKE THE BOT SMARTER
+     */
+    /*
+     * Default stupid behavior: Think (sleep) for 3 seconds, and then return a
+     * sequence
+     * that just moves left and right repeatedly.
+     */
+    return "rrrddlllrrruullldurrrddlludrruuldllur";
+  }
+
+}
