1bf3f96054e9a3e4675b06fbe07f2ab0a3b774db
Sorrypre <jjlespada5@gmail.com>
2025-10-17 18:21:05 +0800

    added node class. Still thinking if A* should be a class on itself and just called in the sokobot class

---
+++ sokobot/src/solver/Node.java
@@ -0,0 +1,24 @@
+package solver;
+
+public class Node {
+    public Node(GameState state, Node parent, int gScore, int hScore) {
+        this.state = state;
+        this.parent = parent;
+        // key component for guiding A* search algorithm
+        this.fScore = gScore + hScore;
+    }
+    public GameState getState() {
+        return state;
+    }
+    // getter for parent node or the previous state encapsulated in the node (yes di ko na alam )
+    public Node getParent() {
+        return parent;
+    }
+    public int getFScore() {
+        return fScore;
+    }
+
+    private final GameState state;
+    private final Node parent;
+    private final int fScore;
+}
